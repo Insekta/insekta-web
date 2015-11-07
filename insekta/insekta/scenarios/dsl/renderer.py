d5ddc58cddac19dfaa4ec6027c72014f0308e22b
Henning Pridöhl <henning@hprid.de>
2015-11-07 21:33:18 +0100

    scenarios: Fix choice call in renderer

--- insekta/insekta/scenarios/dsl/renderer.py
+++ insekta/insekta/scenarios/dsl/renderer.py
@@ -108,8 +108,8 @@ class Renderer:
                 extra += ' checked'
         else:
             add_check = (self._is_submitted_task() and
-                         (choice_type == 'checkbox' and self.submitted_values[name]) or
-                         (choice_type == 'radio' and self.submitted_values['answer'] == name))
+                         ((choice_type == 'checkbox' and self.submitted_values[name]) or
+                          (choice_type == 'radio' and self.submitted_values['answer'] == name)))
             if add_check:
                 extra += ' checked'
         if choice_type == 'radio':
