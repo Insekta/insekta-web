579e3f7a0ba5f6c71595a97fa4133f9f004fa7ce
Henning Pridöhl <henning@hprid.de>
2015-12-02 18:24:24 +0100

    scenario: Add mark_safe to scenario_progress

--- insekta/insekta/scenarios/templatetags/scenario_progress.py
+++ insekta/insekta/scenarios/templatetags/scenario_progress.py
@@ -1,6 +1,6 @@
 from django import template
 from django.conf import settings
-
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
@@ -13,7 +13,7 @@ def scenario_progress(num_submitted, num_available):
                   'title="Solved">').format(settings.STATIC_URL)
 
     num_missing = num_available - num_submitted
-    return solved_dot * num_submitted + unsolved_dot * num_missing
+    return mark_safe(solved_dot * num_submitted + unsolved_dot * num_missing)
 
 
 scenario_progress.is_safe = True
