c2b57927c3969fdfdf40b878bb3f004d38e26147
Henning Pridöhl <henning@hprid.de>
2016-01-20 10:57:43 +0100

    Improve navigation on account page

--- insekta/insekta/account/views.py
+++ insekta/insekta/account/views.py
@@ -1,9 +1,6 @@
-from django.conf import settings
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 @login_required
 def index(request):
-    return render(request, 'account/index.html', {
-        'show_vpn': 'insekta.vpn' in settings.INSTALLED_APPS
-    })
+    return render(request, 'account/index.html')
