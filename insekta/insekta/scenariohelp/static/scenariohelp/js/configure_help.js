ac55488d20daaab1c1721c6aae6f5b8701267ddb
Henning Pridöhl <henning@hprid.de>
2016-01-23 01:42:05 +0100

    Fix javascript of configure help page

--- insekta/insekta/scenariohelp/static/scenariohelp/js/configure_help.js
+++ insekta/insekta/scenariohelp/static/scenariohelp/js/configure_help.js
@@ -1,14 +1,13 @@
 $(function() {
     function changeSupport(ev) {
         var checkbox = $(this);
-        var label = checkbox.parent();
         $.post(SET_SUPPORT_URL, {
             'scenario': checkbox.attr('name'),
             'enabled': checkbox.prop('checked') ? '1' : '0'
         });
     }
 
-    $('#chosen-scenarios').find('input[type="checkbox"]').each(function() {
+    $('#chosen-scenarios input[type="checkbox"]').each(function() {
         $(this).on('change', changeSupport);
-    })
+    });
 });
