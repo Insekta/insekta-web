52237bf566ac8c459e6e9fd5cc1f966b27f30b48
Henning Pridöhl <henning@hprid.de>
2016-01-25 13:16:15 +0100

    Better admin for remoteapi

--- insekta/insekta/remoteapi/admin.py
+++ insekta/insekta/remoteapi/admin.py
@@ -3,4 +3,8 @@ from django.contrib import admin
 from insekta.remoteapi.models import VMResourceDummy
 
 
-admin.site.register(VMResourceDummy)
+class VMResourceDummyAdmin(admin.ModelAdmin):
+    list_display = ('resource_name', 'is_started', 'vpn_enabled', 'vm_names')
+
+
+admin.site.register(VMResourceDummy, VMResourceDummyAdmin)
