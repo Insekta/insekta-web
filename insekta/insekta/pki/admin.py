1f7fe4f123c172760206272f2a2a61e28c437e54
Henning Pridöhl <henning@hprid.de>
2016-01-25 13:13:46 +0100

    Better admin for pki

--- insekta/insekta/pki/admin.py
+++ insekta/insekta/pki/admin.py
@@ -3,5 +3,8 @@ from django.contrib import admin
 from insekta.pki.models import Certificate
 
 
-admin.site.register(Certificate)
+class CertificateAdmin(admin.ModelAdmin):
+    list_display = ('user', 'fingerprint', 'expires', 'is_revoked')
+    list_filter = ('is_revoked', )
 
+admin.site.register(Certificate, CertificateAdmin)
