490e4645239a1fafdf5177366c9abcb6d41c16cd
Henning Pridöhl <henning@hprid.de>
2016-02-13 17:55:27 +0100

    Add simple JavaScript templating function

--- insekta/insekta/base/static/js/base.js
+++ insekta/insekta/base/static/js/base.js
@@ -66,6 +66,52 @@ function escapeHtml(s) {
     });
 }
 
+// Simple JavaScript Templating
+// John Resig - http://ejohn.org/ - MIT Licensed
+// modified to use [% %] instead of <% %>,  check for [% instead of \W for element-id or string template
+/*
+ * Use it like:
+ * <script type="text/html" id="users_template">
+ *    [[ for (var i = 0; i < users.length; i++) { ]]
+ *        <a href="[[=users[i].urk]]">[[=escapeHtml(users[i].name)]]<a/>
+ *    [[ } ]]
+ * </script>
+ * tmpl('users_template', {'users': [...]})
+ */
+(function () {
+    var cache = {};
+
+    this.tmpl = function tmpl(str, data) {
+        // Figure out if we're getting a template, or if we need to
+        // load the template - and be sure to cache the result.
+        var fn = !/\[%/.test(str) ?
+            cache[str] = cache[str] ||
+                tmpl(document.getElementById(str).innerHTML) :
+
+            // Generate a reusable function that will serve as a template
+            // generator (and which will be cached).
+            new Function("obj",
+                "var p=[],print=function(){p.push.apply(p,arguments);};" +
+
+                    // Introduce the data as local variables using with(){}
+                "with(obj){p.push('" +
+
+                    // Convert the template into pure JavaScript
+                str
+                    .replace(/[\r\t\n]/g, " ")
+                    .split("[%").join("\t")
+                    .replace(/((^|%\])[^\t]*)'/g, "$1\r")
+                    .replace(/\t=(.*?)%\]/g, "',$1,'")
+                    .split("\t").join("');")
+                    .split("%]").join("p.push('")
+                    .split("\r").join("\\'")
+                + "');}return p.join('');");
+
+        // Provide some basic currying to the user
+        return data ? fn(data) : fn;
+    };
+})();
+
 jQuery.fn.center = function () {
     var jWindow = $(window);
     var topValue = Math.max(0, (jWindow.height() - this.outerHeight()) / 2 + jWindow.scrollTop());
